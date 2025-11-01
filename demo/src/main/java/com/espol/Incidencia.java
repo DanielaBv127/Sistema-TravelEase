ce84ee7a09b4fb1d29e2460584491948e4b7ee08
DanielaBv127 <marbozza@espol.edu.ec>
2025-11-01 18:32:29 -0500

    Creación de clases "Incidencia", "Proveedor", "Usuario"

--- demo/src/main/java/com/espol/Incidencia.java
+++ demo/src/main/java/com/espol/Incidencia.java
@@ -1,5 +1,56 @@
 package com.espol;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Incidencia {
-    
+    private int idIncidencia;
+    private String descripcion;
+    private EstadoIncidencia estado;
+    private Pasajero pasajeroReporta;
+    private AgenteSoporte agenteAsignado;
+    private List<Escalamiento> escalamientos;
+
+    public Incidencia(int idIncidencia, String descripcion, Pasajero pasajeroReporta){
+        this.idIncidencia= idIncidencia;
+        this.descripcion= descripcion;
+        this.pasajeroReporta= pasajeroReporta;
+        this.estado= EstadoIncidencia.ABIERTA;
+        this.escalamientos= new ArrayList<>();
+    }
+
+    public void registrarIncidencia(){
+        System.out.println("Incidencia "+ idIncidencia+" registrada por "+pasajeroReporta,getNombre()+". Estado: "+this.estado);
+    }
+
+    public void resolverIncidencia(){
+        if (agenteAsignado==null){
+            System.out.println("Error: La incidencia no puede cerrarse sin un agente asignado.");
+            return;
+        }
+        this.estado= EstadoIncidencia.CERRADA;
+        System.out.println("Incidencia "+ idIncidencia+" resuelta por "+ agenteAsignado.getNombre());
+    }
+
+    public void puedeEscalar(Escalamiento escalamiento){
+        this.escalamientos.add(escalamiento);
+        this.estado= EstadoIncidencia.EN_PROCESO;
+        escalamiento.escalar();
+    }
+
+    public int getIdIncidencia(){
+        return idIncidencia;
+    }
+
+    public EstadoIncidencia getEstado(){
+        return estado;
+    }
+
+    public void setEstado(EstadoIncidencia estado){
+        this.estado= estado;
+    }
+
+    public void setAgenteAsignado(AgenteSoporte agenteAsignado){
+        this.agenteAsignado= agenteAsignado;
+    }
 }
--- demo/src/main/java/com/espol/Proveedor.java
+++ demo/src/main/java/com/espol/Proveedor.java
@@ -1,5 +1,21 @@
 package com.espol;
 
-public class Proveedor {
-    
+public abstract class Proveedor {
+    private int idProveedor;
+    private String nombre;
+    private String tipoServicio;
+
+    public Proveedor(int idProveedor, String nombre, String tipoServicio){
+        this.idProveedor= idProveedor;
+        this.nombre= nombre;
+        this.tipoServicio= tipoServicio;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public int getIdProveedor(){
+        return idProveedor;
+    }
 }
