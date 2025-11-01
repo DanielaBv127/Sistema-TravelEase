44e4abb49721dc99eb6e351e0d0960bd8d8315eb
Daniela Astudillo León <danvastu@espol.edu.ec>
2025-11-01 17:57:24 -0500

    Creacion de clases Pasajero, Administrador, AgenteSoporte y ServicioAdicional
    Creacion de las clases junto a sus implementaciones

--- demo/src/main/java/com/espol/Administrador.java
+++ demo/src/main/java/com/espol/Administrador.java
@@ -1,5 +1,28 @@
-package com.espol;
+import com.espol.Proveedor;
+import com.espol.Usuario;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Administrador {
+public class Administrador extends Usuario {
     
+    private List<Proveedor> proveedores;
+
+    public Administrador(int id, String nombre, String correo, String contrasena) {
+        super(id, nombre, correo, contrasena);
+        this.proveedores = new ArrayList<>();
+    }
+
+    public void gestionarPoliticasCancelacion() {
+        System.out.println("Admin " + nombre + ": Actualizando políticas de cancelación.");
+    }
+
+    public void gestionarReembolsos() {
+        System.out.println("Admin " + nombre + ": Procesando reembolsos maestros.");
+    }
+
+    public void integrarProveedores(Proveedor proveedor) {
+        if (proveedor == null) return;
+        this.proveedores.add(proveedor);
+        System.out.println("Proveedor " + proveedor.getNombre() + " integrado al sistema.");
+    }
 }
