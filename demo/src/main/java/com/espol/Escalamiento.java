975dcdc2c7a3335acdff1b586b95697e6aab8789
DanielaBv127 <marbozza@espol.edu.ec>
2025-11-01 19:26:21 -0500

    Ajustando clase Incidencia y Escalamiento

--- demo/src/main/java/com/espol/Escalamiento.java
+++ demo/src/main/java/com/espol/Escalamiento.java
@@ -1,5 +1,31 @@
 package com.espol;
 
+import java.util.Date;
+
 public class Escalamiento {
-    
+    private int nivel;
+    private Date fechaEscalamiento;
+    private Proveedor proveedor;
+
+    public Escalamiento(int nivel, Proveedor proveedor) {
+        this.nivel = nivel;
+        this.proveedor = proveedor;
+        this.fechaEscalamiento = new Date();
+    }
+
+    public void escalar() {
+        System.out.println("ACCIÓN: Escalando incidencia (Nivel " + this.nivel + ") al proveedor: " + proveedor.getNombre());
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public Date getFechaEscalamiento() {
+        return fechaEscalamiento;
+    }
+
+    public Proveedor getProveedor() {
+        return proveedor;
+    }
 }
--- demo/src/main/java/com/espol/Reserva.java
+++ demo/src/main/java/com/espol/Reserva.java
@@ -1,5 +1,9 @@
 package com.espol;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class Reserva {
     
 }
